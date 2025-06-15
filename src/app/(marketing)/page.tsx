dead92f22f03fd022159af75bb7fcb2a0f38b793
venkatsundaraj <venkateshsundarasan@gmail.com>
2025-06-15 23:43:43 +0530

    heading

--- src/app/(marketing)/page.tsx
+++ src/app/(marketing)/page.tsx
@@ -3,5 +3,13 @@ import TaskList from "~/app/_components/task-list";
 import { api } from "~/trpc/react";
 
 export default async function Home() {
-  return <></>;
+  return (
+    <section className="flex min-h-screen w-screen items-center justify-center">
+      <div className="container">
+        <h1 className="font-heading text-primary w-full text-center text-4xl md:text-9xl">
+          Snap. Suggest. Buy.
+        </h1>
+      </div>
+    </section>
+  );
 }
--- src/app/_components/header.tsx
+++ src/app/_components/header.tsx
@@ -17,7 +17,7 @@ const Header: FC<HeaderProps> = () => {
     >
       <div className="flex w-full items-center justify-between">
         <Link href={"/"}>
-          <Icons.Sprout className="stroke-primary fill-primary h-12 w-12" />
+          <Icons.Sprout className="stroke-foreground fill-primary h-12 w-12 stroke-1" />
         </Link>
         <CommandMenu />
       </div>
